e8e41cdd67ec953aa18e00efe9ee99d2ae42c473
Manhik098 <manhikkalyan@gmail.com>
2026-01-14 23:34:55 +0530

    Updated login, register and dashboard UI

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://task-manager-mu-virid.vercel.app"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("API Running"));
